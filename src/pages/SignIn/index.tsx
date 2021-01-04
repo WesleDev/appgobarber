86e29f2dab3e304cd2ca33db89aaa729f59f07b2
Wesle Leal <wesle.profissional@gmail.com>
2021-01-04 08:45:07 -0300

    Input e Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
@@ -10,6 +13,18 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="Email" />
+
+      <Input name="email" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('teste');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
